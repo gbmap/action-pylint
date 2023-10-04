dcdcd7fe095e4c74da44700e5a35ae40861cfe80
gbmap <gabehilism@gmail.com>
2023-10-04 09:09:05 -0300

    added pylint-options to action; refactored code.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let fail = true;
 let pr_message = true;
 let GITHUB_TOKEN = '';
 let output = '';
+let pylint_options = '';
 const default_no_error_message = 'No lint errors found';
 
 function commentPr(message, token) {
@@ -21,31 +22,13 @@ function parseLintMessage(lint_message) {
     return `${lint_message.type} [${lint_message['message-id']}] ${lint_message.message} (${lint_message.path}:${lint_message.line}:${lint_message.column})`;
 }
 
-function buildMessage(pylint_errors, pylint_warnings, pylint_info, pylint_convention, pylint_refactor) {
-    let message = '';
-    if (pylint_errors.length > 0) {
-        message += `===> ${pylint_errors.length} error(s) found:\n`;
-        pylint_errors.forEach(error => message += `- ${parseLintMessage(error)}\n`);
+function buildMessage(msgs, type) {
+    if (msgs.length == 0) {
+        return '';
     }
-    if (pylint_warnings.length > 0) {
-        message += `===> ${pylint_warnings.length} warning(s) found:\n`;
-        pylint_warnings.forEach(warning => message += `- ${parseLintMessage(warning)}\n`);
-    }
-    if (pylint_info.length > 0) {
-        message += `===> ${pylint_info.length} info(s) found:\n`;
-        pylint_info.forEach(info => message += `- ${parseLintMessage(info)}\n`);
-    }
-    if (pylint_convention.length > 0) {
-        message += `===> ${pylint_convention.length} convention(s) found:\n`;
-        pylint_convention.forEach(convention => message += `- ${parseLintMessage(convention)}\n`);
-    }
-    if (pylint_refactor.length > 0) {
-        message += `===> ${pylint_refactor.length} refactor(s) found:\n`;
-        pylint_refactor.forEach(refactor => message += `- ${parseLintMessage(refactor)}\n`);
-    }
-    if (message === '') {
-        message = default_no_error_message;
-    }
+
+    let message = `===> ${msgs.length} ${type}(s) found:\n`;
+    msgs.forEach(msg => message += `- ${parseLintMessage(msg)}\n`);
     return message;
 }
 
@@ -55,6 +38,7 @@ async function run() {
         const path = core.getInput('path');
         fail = core.getInput('fail');
         pr_message = core.getInput('pr-message');
+        pylint_options = core.getInput('pylint-options');
         GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
         // Install pylint
@@ -70,19 +54,20 @@ async function run() {
                 output += data.toString();
             }
         }
-        await exec.exec('/bin/bash', ['-c', `pylint ${path} -f json`], options);
+        await exec.exec('/bin/bash', ['-c', `pylint ${path} -f json ${pylint_options}`], options);
     } catch (error) {
         // Parse pylint output
         const pylint_output = JSON.parse(output);
-        const pylint_errors = pylint_output.filter(message => message.type == 'error');
-        const pylint_warnings = pylint_output.filter(message => message.type == 'warning');
-        const pylint_info = pylint_output.filter(message => message.type == 'info');
-        const pylint_convention = pylint_output.filter(message => message.type == 'convention');
-        const pylint_refactor = pylint_output.filter(message => message.type == 'refactor');
 
-        // Builds message
-        const message = buildMessage(pylint_errors, pylint_warnings, pylint_info, pylint_convention, pylint_refactor);
-        console.log(message);
+        message_types = ['error', 'warning', 'info', 'convention', 'refactor'];
+
+        final_msg = '';
+        for (msg_type in message_types) {
+            msgs = pylint_output.filter(message => message.type == msg_type);
+            msg = buildMessage(msgs, msg_type);
+            final_msg += msg;
+        }
+        console.log(final_msg);
 
         // Comment on PR
         if ((pr_message) && (message !== default_no_error_message)) {
